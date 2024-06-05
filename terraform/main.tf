8438921a0448ad56fba48800eb2d30ff316b320c
NorthCountryEngineer <eric@northcountryengineer.com>
2024-06-05 12:36:49 -0400

    reformatting to handle an exported NEXT site

--- terraform/main.tf
+++ terraform/main.tf
@@ -16,30 +16,6 @@ provider "aws" {
     region  = var.aws_region
 }
 
-//CLOUDFLARE
-/* Commented out as it throws an error if the record already exists
-provider "cloudflare" {
-    api_token = var.cloudflare_api_token
-}
-
-resource "cloudflare_record" "website" {
-    zone_id = "your_cloudflare_zone_id"
-    name    = "your_domain.com"
-    type    = "CNAME"
-    value   = "http://upstate-tech.dev.s3-website-us-east-1.amazonaws.com/"
-    ttl     = 3600
-}
-
-
-resource "cloudflare_record" "www" {
-    zone_id = var.zone_id
-    name    = "www"
-    value   = "203.0.113.10"
-    type    = "A"
-    proxied = true
-}
-*/
-
 //AWS S3
 resource "aws_s3_bucket" "site" {
     bucket = var.site_domain
@@ -106,3 +82,22 @@ resource "aws_s3_bucket_policy" "site" {
         aws_s3_bucket_public_access_block.site
     ]
 }
+
+//CLOUDFLARE SETUP REQUIRED FOR FIRST DEPLOYMENT TO NEW ENVIRONMENT, COMMENTED
+//DUE TO LIMITATION WITH TERRAFORM AS IT WILL FAIL IF A RECORD ALREADY EXISTS
+
+/* 
+provider "cloudflare" {
+    api_token = var.cloudflare_api_token
+}
+
+resource "cloudflare_record" "website" {
+    zone_id = ${{var.zone_id}}
+    name    = ${{var.site_id}}
+    type    = "CNAME"
+    value   = "http://${{var.site_id}}.s3-website-us-east-1.amazonaws.com/"
+    proxied = true
+    ttl     = 3600
+}
+
+*/
