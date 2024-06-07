00c61314012ee6ac14665b00f2f44b2cf9d10138
NorthCountryEngineer <eric@northcountryengineer.com>
2024-06-07 14:24:45 -0400

    OIDC for workflows

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,13 +1,3 @@
-
-terraform {
-    required_providers {
-        aws = {
-            source  = "hashicorp/aws"
-            version = "~> 4.0"
-        }
-    }
-}
-
 provider "aws" {
     region = var.aws_region
     access_key = var.aws_access_key
