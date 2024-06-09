f9159a8fbc688b6478d314ed957f3b9ebf5ae2e1
NorthCountryEngineer <eric@northcountryengineer.com>
2024-06-09 08:10:46 -0400

    working on CI/CD in new repo/org

--- terraform/static_site/src/app/page.tsx
+++ terraform/static_site/src/app/page.tsx
@@ -47,7 +47,7 @@ export default function Home() {
           rel="noopener noreferrer"
         >
           <h2>
-            Docs <span>-&gt;</span>
+            Spocks <span>-&gt;</span>
           </h2>
           <p>Find in-depth information about Next.js features and API.</p>
         </a>
