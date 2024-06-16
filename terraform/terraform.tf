65c029f16e4c7672e1fc6262ba86e9552419e3e7
NorthCountryEngineer <eric@northcountryengineer.com>
2024-06-16 08:57:52 -0400

    spinning up resources for auth lambda

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -1,13 +1,17 @@
 terraform {  
     required_version = "~> 1.6"
     required_providers {
-        aws = {      
-            source  = "hashicorp/aws"      
-            version = "4.67.0"    
+        aws = {
+            source  = "hashicorp/aws"
+            version = "~> 5.38.0"
         }
         cloudflare = {
             source  = "cloudflare/cloudflare"
             version = ">= 4.20"
+        }
+        archive = {
+            source  = "hashicorp/archive"
+            version = "~> 2.4.2"
         }
     }
 }
