ee35dca22d021f81a64539db92a009dfdcb8c96d
NorthCountryEngineer <eric@northcountryengineer.com>
2024-06-16 09:08:59 -0400

    spinning up resources for auth lambda

--- terraform/authentication.tf
+++ terraform/authentication.tf
@@ -157,76 +157,71 @@ resource "aws_lambda_function" "authentication_function" {
     }
 }
 
+// APIG
 
-/*
-
-resource "aws_api_gateway_rest_api" "auth_api" {
-    name        = "auth_api"
-    description = "API for user authentication"   
+resource "aws_apigatewayv2_api" "lambda" {
+    name          = "serverless_lambda_gw"
+    protocol_type = "HTTP"
 }
 
-resource "aws_api_gateway_resource" "signup" {
-    rest_api_id = aws_api_gateway_rest_api.auth_api.id
-    parent_id   = aws_api_gateway_rest_api.auth_api.root_resource_id
-    path_part   = "signup"
+resource "aws_apigatewayv2_stage" "lambda" {
+    api_id = aws_apigatewayv2_api.lambda.id
+
+    name        = "serverless_lambda_stage"
+    auto_deploy = true
+
+    access_log_settings {
+        destination_arn = aws_cloudwatch_log_group.api_gw.arn
+
+        format = jsonencode({
+            requestId               = "$context.requestId"
+            sourceIp                = "$context.identity.sourceIp"
+            requestTime             = "$context.requestTime"
+            protocol                = "$context.protocol"
+            httpMethod              = "$context.httpMethod"
+            resourcePath            = "$context.resourcePath"
+            routeKey                = "$context.routeKey"
+            status                  = "$context.status"
+            responseLength          = "$context.responseLength"
+            integrationErrorMessage = "$context.integrationErrorMessage"
+        }
+        )
+    }
 }
 
-resource "aws_api_gateway_resource" "signin" {
-    rest_api_id = aws_api_gateway_rest_api.auth_api.id
-    parent_id   = aws_api_gateway_rest_api.auth_api.root_resource_id
-    path_part   = "signin"
+resource "aws_apigatewayv2_integration" "hello_world" {
+    api_id = aws_apigatewayv2_api.lambda.id
+
+    integration_uri    = aws_lambda_function.authentication_function.invoke_arn
+    integration_type   = "AWS_PROXY"
+    integration_method = "POST"
 }
 
-resource "aws_api_gateway_method" "signup_method" {
-    rest_api_id   = aws_api_gateway_rest_api.auth_api.id
-    resource_id   = aws_api_gateway_resource.signup.id
-    http_method   = "POST"
-    authorization = "NONE"
+resource "aws_apigatewayv2_route" "hello_world" {
+    api_id = aws_apigatewayv2_api.lambda.id
+
+    route_key = "GET /hello"
+    target    = "integrations/${aws_apigatewayv2_integration.hello_world.id}"
 }
 
-resource "aws_api_gateway_method" "signin_method" {
-    rest_api_id   = aws_api_gateway_rest_api.auth_api.id
-    resource_id   = aws_api_gateway_resource.signin.id
-    http_method   = "POST"
-    authorization = "NONE"
+resource "aws_cloudwatch_log_group" "api_gw" {
+    name = "/aws/api_gw/${aws_apigatewayv2_api.lambda.name}"
+
+    retention_in_days = 30
 }
 
-resource "aws_api_gateway_integration" "signup_integration" {
-    rest_api_id             = aws_api_gateway_rest_api.auth_api.id
-    resource_id             = aws_api_gateway_resource.signup.id
-    http_method             = aws_api_gateway_method.signup_method.http_method
-    integration_http_method = "POST"
-    type                    = "AWS_PROXY"
-    uri                     = aws_lambda_function.auth_function.invoke_arn
-}
-
-resource "aws_api_gateway_integration" "signin_integration" {
-    rest_api_id             = aws_api_gateway_rest_api.auth_api.id
-    resource_id             = aws_api_gateway_resource.signin.id
-    http_method             = aws_api_gateway_method.signin_method.http_method
-    integration_http_method = "POST"
-    type                    = "AWS_PROXY"
-    uri                     = aws_lambda_function.auth_function.invoke_arn
-}
-
-resource "aws_lambda_permission" "api_gateway" {
-    statement_id  = "AllowAPIGatewayInvoke"
+resource "aws_lambda_permission" "api_gw" {
+    statement_id  = "AllowExecutionFromAPIGateway"
     action        = "lambda:InvokeFunction"
-    function_name = aws_lambda_function.auth_function.function_name
+    function_name = aws_lambda_function.authentication_function.function_name
     principal     = "apigateway.amazonaws.com"
-    source_arn    = "${aws_api_gateway_rest_api.auth_api.execution_arn}/* /*" REMOVE SPACE
+
+    source_arn = "${aws_apigatewayv2_api.lambda.execution_arn}/*/*"
 }
 
-resource "aws_api_gateway_deployment" "auth_api_deployment" {
-    depends_on = [
-        aws_api_gateway_integration.signup_integration,
-        aws_api_gateway_integration.signin_integration
-    ]
-    rest_api_id = aws_api_gateway_rest_api.auth_api.id
-    stage_name  = "prod"
-}
 
-*/
+
+
 // Outputs 
 
 output "cognito_user_pool_id" {
