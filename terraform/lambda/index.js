354041494e4c396c0cfd6a5bd07d107a384cd003
NorthCountryEngineer <eric@northcountryengineer.com>
2024-06-16 09:03:59 -0400

    spinning up resources for auth lambda

--- terraform/lambda/index.js
+++ terraform/lambda/index.js
@@ -1,62 +1,14 @@
-const AWS = require('aws-sdk');
-const Cognito = new AWS.CognitoIdentityServiceProvider();
-
-exports.handler = async (event) => {
-    const body = JSON.parse(event.body);
-
-    if (event.resource === '/signup' && event.httpMethod === 'POST') {
-        const params = {
-            ClientId: process.env.COGNITO_CLIENT_ID,
-            Username: body.email,
-            Password: body.password,
-            UserAttributes: [
-                {
-                    Name: 'email',
-                    Value: body.email,
-                },
-            ],
-        };
-
-        try {
-            const result = await Cognito.signUp(params).promise();
-            return {
-                statusCode: 200,
-                body: JSON.stringify(result),
-            };
-        } catch (error) {
-            return {
-                statusCode: 400,
-                body: JSON.stringify({ error: error.message }),
-        };
-        }
-    }
-
-    if (event.resource === '/signin' && event.httpMethod === 'POST') {
-        const params = {
-            AuthFlow: 'USER_PASSWORD_AUTH',
-            ClientId: process.env.COGNITO_CLIENT_ID,
-            AuthParameters: {
-                USERNAME: body.email,
-                PASSWORD: body.password,
-            },
-        };
-
-        try {
-            const result = await Cognito.initiateAuth(params).promise();
-            return {
-                statusCode: 200,
-                body: JSON.stringify(result),
-            };
-        } catch (error) {
-            return {
-                statusCode: 400,
-                body: JSON.stringify({ error: error.message }),
-            };
-        }
-    }
+module.exports.handler = async (event) => {
+    console.log('Event: ', event);
+    let responseMessage = 'Hello, World!';
 
     return {
-        statusCode: 404,
-        body: JSON.stringify({ error: 'Not Found' }),
-    };
-};
+        statusCode: 200,
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            message: responseMessage,
+        }),
+    }
+}
