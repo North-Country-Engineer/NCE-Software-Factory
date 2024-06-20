14cc816c98ba21dfe7dbb0c92ed51ae3024d2848
NorthCountryEngineer <eric@northcountryengineer.com>
2024-06-20 15:40:28 -0400

    routing stopped working in deployed envs, updating next config

--- terraform/static_site/next.config.mjs
+++ terraform/static_site/next.config.mjs
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
+    async redirects() {
+        return [
+            {
+                source: '/:path*',
+                destination: '/',
+                permanent: true,
+            },
+        ];
+    },
+    trailingSlash: true,
 };
 
 export default nextConfig;
