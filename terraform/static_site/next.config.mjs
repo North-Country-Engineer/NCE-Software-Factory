0e4f9e8895379340553ed4da3042433f008d3221
NorthCountryEngineer <eric@northcountryengineer.com>
2024-06-20 15:48:22 -0400

    routing stopped working in deployed envs, updating next config

--- terraform/static_site/next.config.mjs
+++ terraform/static_site/next.config.mjs
@@ -1,16 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
-    async redirects() {
-        return [
-            {
-                source: '/:path*',
-                destination: '/',
-                permanent: true,
-            },
-        ];
+    trailingSlash: true, // Ensure trailing slashes for all paths
+    exportPathMap: async function (
+        defaultPathMap,
+        { dev, dir, outDir, distDir, buildId }
+    ) {
+        return {
+            '/': { page: '/' },
+            '/signin': { page: '/signin' },
+            '/signup': { page: '/signup' },
+            // Add other routes as necessary
+        };
     },
-    trailingSlash: true,
 };
 
 export default nextConfig;
